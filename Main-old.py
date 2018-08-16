0b5081169252f8c7e9ce1f8078c05d2b9aa5da24
David Scheele <david.scheele2@googlemail.com>
2018-08-16 20:07:59 +0200

    Fixed no queue checking, worked on music loading, made machine independant

---
+++ Main-old.py
@@ -0,0 +1,224 @@
+import pygame, time, glob, random, os
+from random import randrange, sample
+from collections import defaultdict
+
+#os.environ["SDL_VIDEODRIVER"] = "dummy" # or maybe 'fbcon'
+
+currentMusicStyle = "0"
+
+def loadMusicStyles():	
+	with open('./musicstyleorder') as file:
+		musicStyleDict = {}
+		counter = 1
+		for line in file:
+			if line[0] != "#":
+				line = line.replace("\n", "")
+				musicStyleDict[str(counter)] = line
+				counter += 1
+	print(musicStyleDict)
+	return musicStyleDict
+
+musicStyleDict = loadMusicStyles()
+maxMusicVolume = 0.5
+
+warnSound = "./warning.ogg"
+
+pygame.init()
+pygame.display.set_caption('super-mc-audiomix')
+pygame.mixer.init()
+#os.putenv('SDL_VIDEODRIVER', 'fbcon')
+pygame.display.init()
+SCREEN = pygame.display.set_mode((1, 1))
+currentMusicChannel = "a"
+musicChannelA = pygame.mixer.Sound("./silence-1-second.wav").play()
+musicChannelB = pygame.mixer.Sound("./silence-1-second.wav").play()
+currentPlaylist = []
+sfxDict = defaultdict(list)
+
+def getRandomPlaylistOf(directory):
+	finaldir = "./sound/" + directory + "/bgm/*"
+	playlist = []
+	for result in glob.iglob(finaldir):
+		playlist.insert(randrange(len(playlist)+1), result)
+	return playlist
+
+def getNextSongFromPlaylist():
+	global currentPlaylist
+	chosenTitle = currentPlaylist[0]
+	currentPlaylist.remove(currentPlaylist[0])
+	return chosenTitle
+
+def fadeTo(newDirection):
+	global currentPlaylist
+	global currentMusicChannel
+	global musicChannelA
+	global musicChannelB
+	global currentMusicStyle
+	
+	print("getting all the stuff")
+	
+	loadSoundEffects(musicStyleDict[newDirection])
+	currentPlaylist = getRandomPlaylistOf(musicStyleDict[newDirection])
+	
+	print("here we fade...")
+	
+	if currentMusicChannel == "a":
+		currentMusicChannel = "b"
+		print "Switching to B channel"
+		vola = maxMusicVolume
+		volb = 0
+		musicChannelB = pygame.mixer.Sound(getNextSongFromPlaylist()).play()
+		musicChannelB.pause()
+		checkTheQueue()
+		musicChannelB.unpause()
+		while vola > 0:
+			print(vola)
+			vola -= 0.01
+			volb += 0.01
+			musicChannelA.set_volume(vola)
+			musicChannelB.set_volume(volb)
+			time.sleep(0.03)
+		musicChannelB.set_volume(maxMusicVolume)
+	else:
+		currentMusicChannel = "a"
+		print "Switching to A channel"
+		volb = maxMusicVolume
+		vola = 0
+		musicChannelA = pygame.mixer.Sound(getNextSongFromPlaylist()).play()
+		musicChannelA.pause()
+		checkTheQueue()
+		musicChannelA.unpause()
+		while volb > 0:
+			print(volb)
+			volb -= 0.01
+			vola += 0.01
+			musicChannelB.set_volume(volb)
+			musicChannelA.set_volume(vola)
+			time.sleep(0.05)
+		musicChannelA.set_volume(maxMusicVolume)
+	currentMusicStyle = newDirection
+
+def loadSoundEffects(directory):
+	global sfxDict
+	print("loading sfx for ")
+	print(directory)
+	sfxDict = defaultdict(list)
+	finaldir = "./sound/" + directory + "/sfx/"
+	for item in glob.iglob(finaldir + "*"):
+		item = item.replace(finaldir, "")
+		print(item)
+		sfxDict[item[0]].append(item)
+
+def checkTheQueue():
+	global currentPlaylist
+	global currentMusicStyle
+	if currentMusicChannel == "a":
+		if musicChannelA.get_queue() is None:
+			print("queue empty!")
+			if len(currentPlaylist) == 0:
+				print("playlist was empty, makin a new one")
+				currentPlaylist = getRandomPlaylistOf(musicStyleDict[currentMusicStyle])
+			musicChannelA.queue(pygame.mixer.Sound(currentPlaylist[0]))
+			currentPlaylist.remove(currentPlaylist[0])
+			print("done!")
+	else:
+		if musicChannelB.get_queue() is None:
+			print("queue empty!")
+			if len(currentPlaylist) == 0:
+				print("playlist was empty, makin a new one")
+				currentPlaylist = getRandomPlaylistOf(musicStyleDict[currentMusicStyle])
+			musicChannelB.queue(pygame.mixer.Sound(currentPlaylist[0]))
+			currentPlaylist.remove(currentPlaylist[0])
+			print("done!")
+
+def playByKey(key):
+	global sfxDict
+	global musicStyleDict
+	global currentMusicStyle
+	if currentMusicStyle != "0":
+		if key in sfxDict:
+			sfxToPlay = "./sound/" + musicStyleDict[currentMusicStyle] + "/sfx/" + random.choice(sfxDict[key])
+			print("I'll play...")
+			print(sfxToPlay)
+			pygame.mixer.Sound(sfxToPlay).play()
+		else:
+			pygame.mixer.Sound(warnSound).play()
+			print("I have nothing for this key...")
+			print(key)
+	else:
+		pygame.mixer.Sound(warnSound).play()
+		print("No soundeffects before you switch to some music!")
+		
+def turnMusicUp():
+	global maxMusicVolume
+	global currentMusicChannel
+	global musicChannelA
+	global musicChannelB
+	if maxMusicVolume < 0.99:
+		vol = maxMusicVolume
+		maxMusicVolume += 0.1		
+		if currentMusicChannel == "a":
+			while vol < maxMusicVolume:
+				print(vol)
+				vol += 0.01
+				musicChannelA.set_volume(vol)
+				time.sleep(0.03)
+		else:
+			while vol < maxMusicVolume:
+				print(vol)
+				vol += 0.01
+				musicChannelB.set_volume(vol)
+				time.sleep(0.03)
+		print("setting to" + str(maxMusicVolume))
+		musicChannelA.set_volume(maxMusicVolume)
+	else:
+		print("Volume is already at 100%!")
+		
+def turnMusicDown():
+	global maxMusicVolume
+	global currentMusicChannel
+	global musicChannelA
+	global musicChannelB
+	if maxMusicVolume > 0.01:
+		vol = maxMusicVolume
+		maxMusicVolume -= 0.1		
+		if currentMusicChannel == "a":
+			while vol > maxMusicVolume:
+				print(vol)
+				vol -= 0.01
+				musicChannelA.set_volume(vol)
+				time.sleep(0.03)
+		else:
+			while vol > maxMusicVolume:
+				print(vol)
+				vol -= 0.01
+				musicChannelB.set_volume(vol)
+				time.sleep(0.03)
+		musicChannelA.set_volume(maxMusicVolume)
+	else:
+		print("Volume is already at 0%!")
+
+while True:
+	os.system("wmctrl -a super-mc-audiomix")
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			if pygame.key.name(event.key) in musicStyleDict:
+				fadeTo(pygame.key.name(event.key))
+			elif event.key == pygame.K_MINUS:
+				turnMusicDown()
+			elif event.key == pygame.K_PLUS:
+				turnMusicUp()
+			elif event.key == pygame.K_ESCAPE:
+				breaker = True
+				print("Really quit? Enter to quit!")
+				while breaker:
+					for event in pygame.event.get():
+						if event.type == pygame.KEYDOWN:
+							if event.key == pygame.K_RETURN:
+								pygame.quit()
+							else:
+								print("I'll break, then")
+								breaker = False
+								break
+			else:
+				playByKey(pygame.key.name(event.key))
--- Main-old2.py
+++ Main-old2.py
@@ -1,189 +1,155 @@
-import pygame, time, glob, random, os, keyboard, logging
+import pygame, time, glob, random, os, keyboard
 from random import randrange, sample
 from collections import defaultdict
-from os.path import expanduser
-home = os.path.expanduser('~pi')
 
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename=home+'/Raspberry-Pi-Audiomixer/Log.log', level=logging.DEBUG)
-
-breaker = True
+#os.environ["SDL_VIDEODRIVER"] = "dummy" # or maybe 'fbcon'
 
 currentMusicStyle = "0"
 
-logging.info('')
 def loadMusicStyles():	
-	logging.info('Loading Music Styles')
-	with open(home+'/Raspberry-Pi-Audiomixer/musicstyleorder') as file:
+	with open('./musicstyleorder') as file:
 		musicStyleDict = {}
 		counter = 1
 		for line in file:
 			if line[0] != "#":
 				line = line.replace("\n", "")
-				logging.info('Recognized style: %s', line)
 				musicStyleDict[str(counter)] = line
 				counter += 1
+	print(musicStyleDict)
 	return musicStyleDict
 
 musicStyleDict = loadMusicStyles()
 maxMusicVolume = 0.5
 
-warnSound = home+"/Raspberry-Pi-Audiomixer/warning.ogg"
+warnSound = "./warning.ogg"
 
-logging.info('Preinit of mixer')
-pygame.mixer.pre_init(22050, -16, 2, 512)
-logging.info('Init Mixer')
+pygame.init()
+pygame.display.set_caption('super-mc-audiomix')
 pygame.mixer.init()
-currentMusicChannel = "x"
-logging.info('Prepping Channel A')
-musicChannelA = pygame.mixer.Sound(home+"/Raspberry-Pi-Audiomixer/silence-1-second.wav").play()
-logging.info('Prepping Channel B')
-musicChannelB = pygame.mixer.Sound(home+"/Raspberry-Pi-Audiomixer/silence-1-second.wav").play()
+#os.putenv('SDL_VIDEODRIVER', 'fbcon')
+pygame.display.init()
+SCREEN = pygame.display.set_mode((1, 1))
+currentMusicChannel = "a"
+musicChannelA = pygame.mixer.Sound("./silence-1-second.wav").play()
+musicChannelB = pygame.mixer.Sound("./silence-1-second.wav").play()
 currentPlaylist = []
 sfxDict = defaultdict(list)
 
 def getRandomPlaylistOf(directory):
-	logging.info('Getting a Radom Playlist')
-	finaldir = home+"/Raspberry-Pi-Audiomixer/sound/" + directory + "/bgm/*"
+	finaldir = "./sound/" + directory + "/bgm/*"
 	playlist = []
 	for result in glob.iglob(finaldir):
 		playlist.insert(randrange(len(playlist)+1), result)
-	logging.info('Done')
 	return playlist
 
 def getNextSongFromPlaylist():
-	logging.info('Getting next song from playlist')
 	global currentPlaylist
 	chosenTitle = currentPlaylist[0]
 	currentPlaylist.remove(currentPlaylist[0])
-	logging.info('Done')
 	return chosenTitle
 
 def fadeTo(newDirection):
-	logging.info('Fading to a new style')
 	global currentPlaylist
 	global currentMusicChannel
 	global musicChannelA
 	global musicChannelB
 	global currentMusicStyle
-
-	if currentMusicChannel == "x":
-		currentMusicChannel = "a"
 	
-	logging.info('Loading new Soundeffects')
+	print("getting all the stuff")
+	
 	loadSoundEffects(musicStyleDict[newDirection])
-	logging.info('Getting new Music')
 	currentPlaylist = getRandomPlaylistOf(musicStyleDict[newDirection])
 	
-	logging.info('Begin Fading...')
+	print("here we fade...")
 	
 	if currentMusicChannel == "a":
-		logging.info('Fading from A')
 		currentMusicChannel = "b"
+		print "Switching to B channel"
 		vola = maxMusicVolume
 		volb = 0
-		logging.info('Play and pause on B')
 		musicChannelB = pygame.mixer.Sound(getNextSongFromPlaylist()).play()
-		logging.info('Playing the song')
 		musicChannelB.pause()
-		logging.info('paused the song')
-		logging.info('Unpause B')
+		checkTheQueue()
 		musicChannelB.unpause()
 		while vola > 0:
-			logging.info('Fading')
+			print(vola)
 			vola -= 0.01
 			volb += 0.01
-			logging.info('Setting Volumes')
 			musicChannelA.set_volume(vola)
 			musicChannelB.set_volume(volb)
 			time.sleep(0.03)
-		logging.info('Done with fading')
-		checkTheQueue()
 		musicChannelB.set_volume(maxMusicVolume)
 	else:
-		logging.info('Fading from B')
 		currentMusicChannel = "a"
+		print "Switching to A channel"
 		volb = maxMusicVolume
 		vola = 0
-		logging.info('Play and pause on A')
 		musicChannelA = pygame.mixer.Sound(getNextSongFromPlaylist()).play()
-		logging.info('Playing the song')
 		musicChannelA.pause()
-		logging.info('paused the song')
-		logging.info('Unpause A')
+		checkTheQueue()
 		musicChannelA.unpause()
 		while volb > 0:
-			logging.info('Fading')
+			print(volb)
 			volb -= 0.01
 			vola += 0.01
-			logging.info('Setting Volumes')
 			musicChannelB.set_volume(volb)
 			musicChannelA.set_volume(vola)
 			time.sleep(0.05)
-		logging.info('Done with fading')
-		checkTheQueue()
 		musicChannelA.set_volume(maxMusicVolume)
-	logging.info('Setting to a new Directory')
 	currentMusicStyle = newDirection
-	logging.info('All done with the fading')
 
 def loadSoundEffects(directory):
-	logging.info('Loading Soundeffects')
 	global sfxDict
-
+	print("loading sfx for ")
+	print(directory)
 	sfxDict = defaultdict(list)
-	finaldir = home+"Raspberry-Pi-Audiomixer/sound/" + directory + "/sfx/"
+	finaldir = "./sound/" + directory + "/sfx/"
 	for item in glob.iglob(finaldir + "*"):
 		item = item.replace(finaldir, "")
+		print(item)
 		sfxDict[item[0]].append(item)
-	logging.info('Done loading Soundeffects')
 
 def checkTheQueue():
 	global currentPlaylist
 	global currentMusicStyle
 	if currentMusicChannel == "a":
 		if musicChannelA.get_queue() is None:
-			logging.info('Nothing in the Queue of A')
+			print("queue empty!")
 			if len(currentPlaylist) == 0:
-				logging.info('Playist empty, making a new one')
+				print("playlist was empty, makin a new one")
 				currentPlaylist = getRandomPlaylistOf(musicStyleDict[currentMusicStyle])
-			logging.info('Setting a new song')
 			musicChannelA.queue(pygame.mixer.Sound(currentPlaylist[0]))
-			logging.info('Removing from playlist')
 			currentPlaylist.remove(currentPlaylist[0])
-			logging.info('Done')
-	elif currentMusicChannel == "b":
+			print("done!")
+	else:
 		if musicChannelB.get_queue() is None:
-			logging.info('Nothing in the Queue of B')
+			print("queue empty!")
 			if len(currentPlaylist) == 0:
-				logging.info('Playlist empty, making new one')
+				print("playlist was empty, makin a new one")
 				currentPlaylist = getRandomPlaylistOf(musicStyleDict[currentMusicStyle])
-			logging.info('Setting a new song')
 			musicChannelB.queue(pygame.mixer.Sound(currentPlaylist[0]))
-			logging.info('Removing from playlist')
 			currentPlaylist.remove(currentPlaylist[0])
-			logging.info('Done')
+			print("done!")
 
 def playByKey(key):
-	logging.info('Playing something because %s %s', key, ' was pressed')
 	global sfxDict
 	global musicStyleDict
 	global currentMusicStyle
 	if currentMusicStyle != "0":
 		if key in sfxDict:
-			logging.info('I have a sound for that!!')
-			sfxToPlay = home+"/Raspberry-Pi-Audiomixer/sound/" + musicStyleDict[currentMusicStyle] + "/sfx/" + random.choice(sfxDict[key])
-			logging.info('Playing %s', sfxToPlay)
+			sfxToPlay = "./sound/" + musicStyleDict[currentMusicStyle] + "/sfx/" + random.choice(sfxDict[key])
+			print("I'll play...")
+			print(sfxToPlay)
 			pygame.mixer.Sound(sfxToPlay).play()
-			logging.info('I played it, are ya happy?')
 		else:
 			pygame.mixer.Sound(warnSound).play()
-			logging.info('I have nothing for %s', key)
+			print("I have nothing for this key...")
+			print(key)
 	else:
 		pygame.mixer.Sound(warnSound).play()
-		logging.info('No music playing yet, no soundeffects for you')
+		print("No soundeffects before you switch to some music!")
 		
 def turnMusicUp():
-	logging.info('Turn the Volume up!')
 	global maxMusicVolume
 	global currentMusicChannel
 	global musicChannelA
@@ -193,23 +159,22 @@ def turnMusicUp():
 		maxMusicVolume += 0.1		
 		if currentMusicChannel == "a":
 			while vol < maxMusicVolume:
-				logging.info(vol)
+				print(vol)
 				vol += 0.01
 				musicChannelA.set_volume(vol)
 				time.sleep(0.03)
 		else:
 			while vol < maxMusicVolume:
-				logging.info(vol)
+				print(vol)
 				vol += 0.01
 				musicChannelB.set_volume(vol)
 				time.sleep(0.03)
-		logging.info('Setting the Volume at the end')
+		print("setting to" + str(maxMusicVolume))
 		musicChannelA.set_volume(maxMusicVolume)
 	else:
-		logging.info('Volume is already at 100%')
+		print("Volume is already at 100%!")
 		
 def turnMusicDown():
-	logging.info('Turn the Volume down!')
 	global maxMusicVolume
 	global currentMusicChannel
 	global musicChannelA
@@ -219,237 +184,261 @@ def turnMusicDown():
 		maxMusicVolume -= 0.1		
 		if currentMusicChannel == "a":
 			while vol > maxMusicVolume:
-				logging.info(vol)
+				print(vol)
 				vol -= 0.01
 				musicChannelA.set_volume(vol)
 				time.sleep(0.03)
 		else:
 			while vol > maxMusicVolume:
-				logging.info(vol)
+				print(vol)
 				vol -= 0.01
 				musicChannelB.set_volume(vol)
 				time.sleep(0.03)
-		logging.info('Setting Volume at the end')
 		musicChannelA.set_volume(maxMusicVolume)
 	else:
-		logging.info('Volume is already at 100%')
+		print("Volume is already at 0%!")
 
-logging.info('Startup Sound')
-pygame.mixer.Sound(home+'/Raspberry-Pi-Audiomixer/startup-sound.ogg').play()
-logging.info('Entering while loop')
-while breaker:
-	checkTheQueue()
+while True:
+	#activate toggle mode
 	if keyboard.is_pressed('space'):
+		#waiting for key to lift
 		while keyboard.is_pressed('space'):
 			pass
-		logging.info('Space was pressed, waiting for a number to fade to other music')
+		print('Listening for fade....')
+		#waiting for input to change the channel
 		while True:
-			checkTheQueue()
 			if keyboard.is_pressed('1') and '1' in musicStyleDict:
 				while keyboard.is_pressed('1'):
 					pass
-				logging.info('1')
+				print('1')
 				fadeTo('1')
 				break
 			if keyboard.is_pressed('2') and '2' in musicStyleDict:
 				while keyboard.is_pressed('2'):
 					pass
-				logging.info('2')
+				print('2')
 				fadeTo('2')
 				break
 			if keyboard.is_pressed('3') and '3' in musicStyleDict:
 				while keyboard.is_pressed('3'):
 					pass
-				logging.info('3')
+				print('3')
 				fadeTo('3')
 				break
 			if keyboard.is_pressed('4') and '4' in musicStyleDict:
 				while keyboard.is_pressed('4'):
 					pass
-				logging.info('4')
+				print('4')
 				fadeTo('4')
 				break
 			if keyboard.is_pressed('5') and '5' in musicStyleDict:
 				while keyboard.is_pressed('5'):
 					pass
-				logging.info('5')
+				print('5')
 				fadeTo('5')
 				break
 			if keyboard.is_pressed('6') and '6' in musicStyleDict:
 				while keyboard.is_pressed('6'):
 					pass
-				logging.info('6')
+				print('6')
 				fadeTo('6')
 				break
 			if keyboard.is_pressed('7') and '7' in musicStyleDict:
 				while keyboard.is_pressed('7'):
 					pass
-				logging.info('7')
+				print('7')
 				fadeTo('7')
 				break
 			if keyboard.is_pressed('8') and '8' in musicStyleDict:
 				while keyboard.is_pressed('8'):
 					pass
-				logging.info('8')
+				print('8')
 				fadeTo('8')
 				break
 			if keyboard.is_pressed('9') and '9' in musicStyleDict:
 				while keyboard.is_pressed('9'):
 					pass
-				logging.info('9')
+				print('9')
 				fadeTo('9')
 				break
 			if keyboard.is_pressed('0') and '0' in musicStyleDict:
 				while keyboard.is_pressed('0'):
 					pass
-				logging.info('0')
+				print('0')
 				fadeTo('0')
 				break		
 			if keyboard.is_pressed('space'):
 				while keyboard.is_pressed('space'):
 					pass
-				logging.info('Alright, we are not fading then.')
+				print('No more fading then...')
 				break
 	if keyboard.is_pressed('1'):
 		while keyboard.is_pressed('1'):
 			pass
-		logging.info('SFX key 1')
+		print('SFX1')
 		playByKey('1')
 	if keyboard.is_pressed('2'):
 		while keyboard.is_pressed('2'):
 			pass
-		logging.info('SFX key 2')
+		print('SFX2')
 		playByKey('2')
 	if keyboard.is_pressed('3'):
 		while keyboard.is_pressed('3'):
 			pass
-		logging.info('SFX key 3')
+		print('SFX3')
 		playByKey('3')
 	if keyboard.is_pressed('4'):
 		while keyboard.is_pressed('4'):
 			pass
-		logging.info('SFX key 4')
+		print('SFX4')
 		playByKey('4')
 	if keyboard.is_pressed('5'):
 		while keyboard.is_pressed('5'):
 			pass
-		logging.info('SFX key 5')
+		print('SFX5')
 		playByKey('5')
 	if keyboard.is_pressed('6'):
 		while keyboard.is_pressed('6'):
 			pass
-		logging.info('SFX key 6')
+		print('SFX6')
 		playByKey('6')
 	if keyboard.is_pressed('7'):
 		while keyboard.is_pressed('7'):
 			pass
-		logging.info('SFX key 7')
+		print('SFX7')
 		playByKey('7')
 	if keyboard.is_pressed('8'):
 		while keyboard.is_pressed('8'):
 			pass
-		logging.info('SFX key 8')
+		print('SFX8')
 		playByKey('8')
 	if keyboard.is_pressed('9'):
 		while keyboard.is_pressed('9'):
 			pass
-		logging.info('SFX key 9')
+		print('SFX9')
 		playByKey('9')
 	if keyboard.is_pressed('0'):
 		while keyboard.is_pressed('0'):
 			pass
-		logging.info('SFX key 0')
+		print('SFX0')
 		playByKey('0')
 	if keyboard.is_pressed('+'):
 		while keyboard.is_pressed('+'):
 			pass
-		logging.info('LOUDER')
+		print('LOUDER')
 		turnMusicUp()
 	if keyboard.is_pressed('-'):
 		while keyboard.is_pressed('-'):
 			pass
-		logging.info('Shut up a litte?')
+		print('Shut up a little?')
 		turnMusicDown()
-	if keyboard.is_pressed('enter'):
-		while keyboard.is_pressed('enter'):
-			pass
-		rlybreak = True
-		logging.info('Enter was pressed. Really quit? Press Enter Again to quit')
-		while rlybreak:
-			if keyboard.is_pressed('enter'):
-				logging.info('We are out then')
-				breaker = False
-				rlybreak = False
-				break
-			if keyboard.is_pressed('0'):
-				while keyboard.is_pressed('0'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('1'):
-				while keyboard.is_pressed('1'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('2'):
-				while keyboard.is_pressed('2'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('3'):
-				while keyboard.is_pressed('3'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('4'):
-				while keyboard.is_pressed('4'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('5'):
-				while keyboard.is_pressed('5'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('6'):
-				while keyboard.is_pressed('6'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('7'):
-				while keyboard.is_pressed('7'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('8'):
-				while keyboard.is_pressed('8'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-				logging.info('Not quitting yet')
-			if keyboard.is_pressed('9'):
-				while keyboard.is_pressed('9'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-			if keyboard.is_pressed('space'):
-				while keyboard.is_pressed('space'):
-					pass
-				logging.info('Not quitting yet')
-				rlybreak = False
-				break
-
-
+#	if keyboard.is_pressed('enter'):
+#		while keyboard.is_pressed('enter'):
+#			pass
+#		breaker = True
+#		print("Really quit? Enter to quit!")
+#		while breaker:
+#			if keyboard.is_pressed('enter'):
+#				pygame.quit()
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('0'):
+#				while keyboard.is_pressed('0'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('1'):
+#				while keyboard.is_pressed('1'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('2'):
+#				while keyboard.is_pressed('2'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('3'):
+#				while keyboard.is_pressed('3'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('4'):
+#				while keyboard.is_pressed('4'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('5'):
+#				while keyboard.is_pressed('5'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('6'):
+#				while keyboard.is_pressed('6'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('7'):
+#				while keyboard.is_pressed('7'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('8'):
+#				while keyboard.is_pressed('8'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('9'):
+#				while keyboard.is_pressed('9'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#			if keyboard.is_pressed('space'):
+#				while keyboard.is_pressed('space'):
+#					pass
+#				print("I'll break, then")
+#				breaker = False
+#				break
+#
+#
 	else:
 		pass
 	
+	
+	
+	
+	
+#	os.system("wmctrl -a super-mc-audiomix")
+#	for event in pygame.event.get():
+#		if event.type == pygame.KEYDOWN:
+#			if event.key == pygame.SPACE:
+#				while true:
+#				if pygame.key.name(event.key) in musicStyleDict:
+#					fadeTo(pygame.key.name(event.key))
+#			elif event.key == pygame.K_MINUS:
+#				turnMusicDown()
+#			elif event.key == pygame.K_PLUS:
+#				turnMusicUp()
+#			elif event.key == pygame.K_ESCAPE:
+#				breaker = True
+#				print("Really quit? Enter to quit!")
+#				while breaker:
+#					for event in pygame.event.get():
+#						if event.type == pygame.KEYDOWN:
+#							if event.key == pygame.K_RETURN:
+#								pygame.quit()
+#							else:
+#								print("I'll break, then")
+#								breaker = False
+#								break
+#			else:
+#				playByKey(pygame.key.name(event.key))
